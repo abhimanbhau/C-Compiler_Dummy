e8f4a1b79b2a7560eed8eebfcc0c30207ff1f78c
Abhimanbhau Kolte <abhimanbhau@gmail.com>
2016-01-13 18:12:46 +0530

    Initial commit

---
+++ CompilerLab/CompilerLab/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CompilerLab
+{
+    class Program
+    {
+        static List<string> keyWords = new List<string>
+        {
+            "int", "char", "float", "double", "return", "for", "if", "while", "printf", "else", "switch"
+            , "case", "goto", "break", "continue", "return"
+        };
+
+        static List<string> operators = new List<string>
+        {
+            "+", "-", "/", "*", "%", "&", "|", "!", "<", ">", ">=", "<=", "==", "=", "++", "--"
+        };
+
+        static List<string> arithmeticOp = new List<string>
+        {
+            "+", "-", "/", "*", "%", "++", "--"
+        };
+        static List<string> logicalOp = new List<string>
+        {
+            "&&", "||", "!="
+        };
+        static List<string> relationalOp = new List<string>
+        {
+            "<", ">", ">=", "<=", "=="
+        };
+        static List<string> bitwiseOp = new List<string>
+        {
+             "&", "|", "!"
+        };
+
+        static void Main(string[] args)
+        {
+            var code = File.ReadAllLines("code.txt");
+
+            // Buffer
+            StringBuilder normalizedBuffer = new StringBuilder();
+
+            StringBuilder tokenBuffer = new StringBuilder();
+
+            foreach (var line in code)
+            {
+                foreach (var character in line)
+                {
+                    if (character == '\t' || character == '\n')
+                    {
+                        normalizedBuffer.Append(" ");
+                    }
+                    else
+                        normalizedBuffer.Append(character);
+                }
+            }
+
+            foreach (var bit in operators)
+                normalizedBuffer.Replace(bit, " " + bit + " ");
+
+            normalizedBuffer.Replace("&  &", "&&");
+            normalizedBuffer.Replace("|  |", "||");
+            normalizedBuffer.Replace("!  =", "!=");
+            normalizedBuffer.Replace("=  =", "==");
+
+            Console.WriteLine(normalizedBuffer + "\n\n");
+
+            var tokensTemp = normalizedBuffer.ToString().Split(new char[] { ' ', ',', '{', '}' });
+
+            var tokensFinal = new List<string>();
+
+            foreach (var token in tokensTemp)
+            {
+                if (token.Trim().Length == 0)
+                    continue;
+
+                if (arithmeticOp.Contains(token))
+                {
+                    tokensFinal.Add("Arithmetic Operator : " + token);
+                }
+                else if (logicalOp.Contains(token))
+                {
+                    tokensFinal.Add("Logical Operator : " + token);
+                }
+                else if (relationalOp.Contains(token))
+                {
+                    tokensFinal.Add("Relational Operator : " + token);
+                }
+                else if (bitwiseOp.Contains(token))
+                {
+                    tokensFinal.Add("Bitwise Operator : " + token);
+                }
+                else if (token == "=")
+                {
+                    tokensFinal.Add("Assignment Operator : " + token);
+                }
+                else if (keyWords.Contains(token))
+                {
+                    tokensFinal.Add("Keyword : " + token);
+                }
+            }
+
+            foreach (var item in tokensFinal)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ LexicalAnalyzer/LexicalAnalyzer/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace LexicalAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Regex ids = new Regex(@"\w*", RegexOptions.Compiled);
+            // var ids = new Regex(@"(([a-z]|[A-Z])+[0-9]*)");
+            //var ids = new Regex("([a-zA-Z][a-zA-Z0-9_]+)*");
+            Regex ops = new Regex(@"[+]|[-]|[*]|[/]|[=]", RegexOptions.Compiled);
+            MatchCollection mc = ids.Matches(input);
+            // Console.WriteLine("Identifiers are");
+
+            foreach (var item in mc)
+            {
+                if (item.ToString().Trim().Length != 0)
+                {
+                    if ((int)item.ToString().Trim()[0] > 64 || item.ToString().Trim()[0] == '_')
+                        Console.WriteLine("Identifier : " + item.ToString());
+                }
+            }
+            MatchCollection opsMatched = ops.Matches(input);
+            Console.WriteLine("\n\n");
+            foreach (var item in opsMatched)
+            {
+                if (item.ToString().Trim().Length != 0)
+                    Console.WriteLine("Operator : " + item.ToString());
+            }
+
+            Regex nums = new Regex(@"\d*");
+            Console.WriteLine("\n\n");
+            var numsMatched = nums.Matches(input);
+            foreach (var item in mc)
+            {
+                if (item.ToString().Trim().Length != 0 && isNumber(item.ToString()))
+                    Console.WriteLine("Number : " + item.ToString());
+            }
+        }
+
+        public static bool isNumber(string input)
+        {
+            return input.All(char.IsDigit);
+        }
+    }
+}
